0dc2a589e1313ffc6077d57c5e2fef9b37d676e1
Mostafa Faouzi <mostafafaouzi16@gmail.com>
2024-10-27 17:29:57 +0100

    first commit

---
+++ src/main/java/com/ENSA/SystemeBiblio/SystemeBiblioApplication.java
@@ -0,0 +1,13 @@
+package com.ENSA.SystemeBiblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeBiblioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeBiblioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ENSA/SystemeBiblio/SystemeBiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ENSA.SystemeBiblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeBiblioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
